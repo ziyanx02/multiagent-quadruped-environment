160d0662c1919199f44d1343380751f34e6ec5a3
ziyanx02 <ziyanx02@gmail.com>
2024-02-05 22:27:15 +0800

    openrl wrapper finished

---
+++ openrl_ws/train.sh
@@ -0,0 +1,3 @@
+python /home/ziyanx/python/multiagent-quadruped-environments/openrl_ws/train.py \
+    --config /home/ziyanx/python/multiagent-quadruped-environments/openrl_ws/cfgs/mqe_ppo.yaml \
+    --num_envs 500 --headless

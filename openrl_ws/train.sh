05fd30e9c3cc08eff223f3d24c1bfb47af3a8ae5
ziyanx02 <14009940+ziyanx02@user.noreply.gitee.com>
2024-02-15 20:32:48 +0800

    set seed

--- openrl_ws/train.sh
+++ openrl_ws/train.sh
@@ -3,5 +3,6 @@ python ./openrl_ws/train.py --headless --num_envs 500 --train_timesteps 30000000
     --algo ppo \
     --sim_device cuda:0 \
     --rl_device cuda:0 \
-    --use_wandb \
-    --exp_name test
+    --seed 1 \
+    # --use_wandb \
+    # --exp_name test
